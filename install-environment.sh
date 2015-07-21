0994abe14213a582c2fdcb7684824e46c5a0070a
cassiobotaro <cassiobotaro@gmail.com>
2015-07-20 22:26:56 -0300

    try to fix emmet

--- install-environment.sh
+++ install-environment.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-setuptools
 sudo apt-get install -y python-setuptools
 sudo easy_install-2.7 pip
-sudo apt-get install python3-pip
+sudo apt-get install -y libboost-python-dev
+sudo apt-get install -y  python3-pip
 
 # libs
 sudo -H pip   install -U virtualenvwrapper
