3f3b96a4de3520cd95f8fd48a59e0fd4fb3d7d2c
cassiobotaro <cassiobotaro@gmail.com>
2015-10-14 00:37:32 -0300

    A little bit more front end

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y git
 sudo apt-get install -y build-essential
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo npm install -g jscs
 
 
 echo "my_sublime requirements are satisfied"
