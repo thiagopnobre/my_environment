21ce27db0c28dedafa804ecc5b27e282c879e4d8
cassiobotaro <cassiobotaro@gmail.com>
2015-10-29 23:59:33 -0200

    typo

--- install-environment.sh
+++ install-environment.sh
@@ -1,4 +1,5 @@
 # softwares
+sudo apt-get update
 sudo apt-get install -y python-dev
 sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-setuptools
@@ -9,7 +10,7 @@ sudo apt-get install -y  python3-pip
 
 # libs
 sudo -H pip   install -U virtualenvwrapper
-sudo -H pip   install jupyter
+sudo -H pip   install -U jupyter
 sudo -H pip   install -U flake8
 sudo -H pip   install -U pytest
 sudo -H pip install -U pep257
