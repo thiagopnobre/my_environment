011356a63d7433ff3480e8d2947104819df2721f
cassiobotaro <cassiobotaro@gmail.com>
2015-11-03 21:55:46 -0200

    new git plugin

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,7 @@
 # git
 sudo apt-get install -y git
 sudo apt-get install -y build-essential
+sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g jscs
