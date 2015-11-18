5255980f23597afb36cf88897566903f0f0fe005
cassiobotaro <cassiobotaro@gmail.com>
2015-11-18 02:11:11 -0200

    Ubuntu 15.10

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,10 +1,5 @@
 # git
 sudo apt-get install -y git
 sudo apt-get install -y build-essential
-sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g jscs
-
 
 echo "my_sublime requirements are satisfied"
